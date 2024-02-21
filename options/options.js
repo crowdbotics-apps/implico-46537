dfd947084b59703309a04d8c4ce04d3072a98502
Crowdbotics <team@crowdbotics.com>
2024-02-21 16:16:52 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "implico-46537",
+  url: "https://implico-46537.botics.co",
+  api: "https://implico-46537.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
